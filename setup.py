547ebf9bd7005972b4c3ec1976db27743efdec44
antlas0 <150958268+antlas0@users.noreply.github.com>
2025-05-23 15:05:16 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LibRelay",
+    version="0.0.1",
+    author="antlas0",
+    description="Small library for the USB-RLY02 from Devantech",
+    packages=find_packages(),
+)
+
